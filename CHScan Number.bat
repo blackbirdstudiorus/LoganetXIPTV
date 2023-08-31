18cf169e872e760a2d0aeb0ab6139bb1e0a6e389
NixIPTV <99161257+blackbirdstudiorus@users.noreply.github.com>
2023-08-31 15:34:32 +0300

    Update CHScan Number.bat

--- CHScan Number.bat
+++ CHScan Number.bat
@@ -1,6 +1,6 @@
-SET /P start="Введите начало:"
-SET /P finish="Введите конец:"
-SET /P min="Введите минимальное число:"
-SET /P max="Введите максимальное число:"
+SET /P start="Start:"
+SET /P finish="Finish:"
+SET /P min="Min:"
+SET /P max="Max:"
 cd C:\Program Files\VideoLAN\VLC
 FOR /L %%N in (%min%,1,%max%) do vlc %start%%%N%finish%
