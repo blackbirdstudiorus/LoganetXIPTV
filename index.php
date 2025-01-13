b9a1b258b60afdafbbeb3c642ad3589808a467be
Loganet IPTV <99161257+blackbirdstudiorus@users.noreply.github.com>
2025-01-13 15:46:51 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,3 +4,4 @@ $page = new DOMDocument();
 $page->loadHTML('<meta http-equiv="Content-Type" content="text/html; charset=utf-8">' . $file);
 $elements = $page->getElementById('cnptype-video').nodeValue;
 print_r($elements)
+?>
