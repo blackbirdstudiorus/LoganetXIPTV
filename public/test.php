089239924866b60aae1bd61ac7584489695e9e0a
Loganet IPTV <99161257+blackbirdstudiorus@users.noreply.github.com>
2025-02-02 11:17:28 +0300

    Update test.php

--- public/test.php
+++ public/test.php
@@ -1,1 +1,7 @@
-
+<?php 
+$file = file_get_contents("https://peers.tv/the_explorers/");
+$page = new DOMDocument();
+$page->loadHTML('<meta http-equiv="Content-Type" content="text/html; charset=utf-8">' . $file);
+$elements = $page->getElementById('cnptype-video').nodeValue;
+print_r($elements)
+?>
